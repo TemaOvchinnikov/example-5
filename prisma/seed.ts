ce98597af31ff65e71632a0ddbf611e5677b103b
Tema Ovchinnikov <flatsplit18@gmail.com>
2021-11-07 00:55:59 +0300

    first commit

---
+++ prisma/seed.ts
@@ -0,0 +1,13 @@
+﻿import { PrismaClient } from '@prisma/client';
+import * as faker from 'faker';
+const prisma = new PrismaClient();
+
+async function main() {}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  //app.use(csurf());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: false,
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+  app.setGlobalPrefix('api');
+  await app.listen(8000);
+}
+bootstrap();
